83cdf5881a3d4fe063ed92604ece16614c7e898a
hyojin shin <dsvit100hjin@gmail.com>
2025-03-31 14:56:55 +0900

    post create/read 기능 업데이트

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -12,10 +13,13 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
+@login_required
 def create(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES) # 사진 데이터도 같이 넣기 위해서 두번째 인자 작성
         if form.is_valid():
+            post = form.save(commit=False) # 이 게시물을 누가 저장했는지의 유저정보가 빠져있음음
+            post.user = request.user
             form.save()
             return redirect('posts:index')
 
