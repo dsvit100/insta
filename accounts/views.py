8a108733add62a3573da9dad989f6ff836712c6d
hyojin shin <dsvit100hjin@gmail.com>
2025-03-31 14:45:47 +0900

    Logout기능구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 
@@ -20,10 +21,11 @@ def signup(request):
 
 def login(request):
     if request.method == 'POST':
-        form = CustomAuthenticationForm(request, request.POST)
+        form = CustomAuthenticationForm(request, request.POST) # request = cookie
         if form.is_valid():
-            user = form.get_user()
-            auth_login(request, user) # 내가 로그인하려는 user에 대한 정보 = user의 id와 pw가 들어있음 -> 폼에서 가져왔으니까
+            user = form.get_user() # form에는 html 태그뭉치를 포함하여 많은 정보가 들어있으므로, 사용자의 id/pw만 가져오게 하는 함수
+            auth_login(request, user)
+            # 내가 로그인하려는 user에 대한 정보 = user의 id와 pw가 들어있음 -> 폼에서 가져왔으니까
             return redirect('posts:index')
     else:
         form = CustomAuthenticationForm()
@@ -31,3 +33,8 @@ def login(request):
         'form': form,
     }
     return render(request, 'login.html', context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('posts:index')
