264e593421a59f47a6233c5ace94aca88a63b127
hyojin shin <dsvit100hjin@gmail.com>
2025-03-31 11:54:22 +0900

    Post Create기능 구현

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -10,3 +11,17 @@ def index(request):
         'posts': posts,
     }
     return render(request, 'index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES) # 사진 데이터도 같이 넣기 위해서 두번째 인자 작성
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+
+    else:
+        form = PostForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'create.html', context)
