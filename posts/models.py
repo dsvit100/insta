7b14f6bd4a059738ecc1f1b581a41c465586a48e
hyojin shin <dsvit100hjin@gmail.com>
2025-03-31 10:03:49 +0900

    admin에 Post모델 등록

--- posts/models.py
+++ posts/models.py
@@ -6,4 +6,4 @@ class Post(models.Model): # 사진 + 내용만 있으니까 title 필요없음
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='image') # upload_to는 필수옵션
-    # 이미지필드를 위해서는 pillow가 필요
+    # 이미지필드를 사용하기 위해서는 pillow가 필요
