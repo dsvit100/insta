4329e7a24924fef235012def69506d320ea6b9b6
hyojin shin <dsvit100hjin@gmail.com>
2025-03-31 09:57:32 +0900

    POST modeling/migration

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model): # 사진 + 내용만 있으니까 title 필요없음
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='image') # upload_to는 필수옵션
+    # 이미지필드를 위해서는 pillow가 필요
