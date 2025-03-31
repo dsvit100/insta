bf187d518458a6c27230e28f8b3b2bf487a8dd96
hyojin shin <dsvit100hjin@gmail.com>
2025-03-31 14:22:15 +0900

    Login 기능 구현

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,10 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import User
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta():
         model = User
         fields = ('username', 'profile_image', ) # 이건 어디서 왔는가
+
+class CustomAuthenticationForm(AuthenticationForm):
+    pass # 커스텀해줄 것 없기 때문에
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 
@@ -15,3 +16,18 @@ def signup(request):
         'form': form,
     }
     return render(request, 'signup.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user) # 내가 로그인하려는 user에 대한 정보 = user의 id와 pw가 들어있음 -> 폼에서 가져왔으니까
+            return redirect('posts:index')
+    else:
+        form = CustomAuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'login.html', context)
