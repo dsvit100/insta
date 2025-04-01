380ee66fea71a44dc373b3ec1aa6e9844ae13d73
hyojin shin <dsvit100hjin@gmail.com>
2025-04-01 11:41:04 +0900

    좋아요 기능 관련 모델링

--- accounts/models.py
+++ accounts/models.py
@@ -11,4 +11,4 @@ class User(AbstractUser):
         size=[500, 500],
         crop=['middle', 'center'],
         upload_to='profile',
-    )
+    ) # 보이지않지만 post_set, comment_set이 들어가있음(1:N)
--- posts/models.py
+++ posts/models.py
@@ -7,6 +7,7 @@ from django.conf import settings
 class Post(models.Model): # 사진 + 내용만 있으니까 title 필요없음
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    # 작성자를 저장하기 위한 필드
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     
     # image = models.ImageField(upload_to='image') # upload_to는 필수옵션
@@ -19,6 +20,14 @@ class Post(models.Model): # 사진 + 내용만 있으니까 title 필요없음
     )
     # 사진을 첨부하면 image 폴더가 생성되고 그 안에 이미지가 저장됨 (upload_to='image')
     # 이미지필드를 사용하기 위해서는 pillow가 필요
+
+    # 중간테이블 = 무엇을 연결해 줄 것인지
+    # 이 글에 좋아요를 누른 사람
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name = 'like_posts'
+        # 역참조에 사용할 이름을 바꾼다 = like_posts로
+    )
 
 
 class Comment(models.Model): # POST와 1:N, 유저와도 1:N
